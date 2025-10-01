f16a41fc84bcc5f3ada5bf06dc31d5920059e910
KeyZhai <1776978150@qq.com>
2025-10-01 23:49:03 +0800

    directory update

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,17 +1,42 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
+import { defineConfig, type CSSOptions } from "vite";
+import { resolve } from "node:path";
+import autoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config
 export default defineConfig(async () => {
-  const vue = (await import('@vitejs/plugin-vue')).default;
-  const tailwindcss = (await import('@tailwindcss/vite')).default;
+  const vue = (await import("@vitejs/plugin-vue")).default;
+  const tailwindcss = (await import("@tailwindcss/vite")).default;
 
   return {
-    plugins: [vue(), tailwindcss()],
+    plugins: [
+      vue(),
+      tailwindcss(),
+      //auto import vue, vue-router, pinia, vue-i18n, @vueuse/core,
+      //dont need to import dependencies explicit
+      autoImport({
+        imports: ["vue", "vue-router", "pinia", "vue-i18n", "@vueuse/core"],
+        dts: "renderer/auto.imports.d.ts",
+      }),
+    ],
+    css: {
+      transformer: "lightningcss" as CSSOptions["transformer"],
+    },
+    build: {
+      target: "es2022",
+      publicDir: "public",
+      input: [
+        resolve(__dirname, "../html/index.html"),
+        resolve(__dirname, "../html/dialog.html"),
+        resolve(__dirname, "../html/settings.html"),
+      ],
+    },
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src')
-      }
-    }
-  }
+        "@common": resolve(__dirname, "common"),
+        "@renderer": resolve(__dirname, "renderer"),
+        "@main": resolve(__dirname, "main"),
+        "@locales": resolve(__dirname, "locales"),
+      },
+    },
+  };
 });
