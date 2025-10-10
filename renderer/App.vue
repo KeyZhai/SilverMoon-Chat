4c6a8210f4c47ae5145178e032d9bb541cd2d196
KeyZhai <feaaizch1001@gmail.com>
2025-10-11 00:16:26 +0800

    feat:add title bar

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import { NConfigProvider } from "naive-ui";
+import TitleBar from "./components/TitleBar.vue";
+import DragRegion from "./components/DragRegion.vue";
 </script>
 
 <template>
   <n-config-provider class="h-[100vh] w-[100vw] flex">
-    <aside class="h-full flex flex-shrink-0 flex-col bg-amber-600">Aside</aside>
-    <div class="auto">Main</div>
+    <aside class="h-full flex flex-shrink-0 flex-col bg-amber-600 w-[200px]">
+      Aside
+    </aside>
+    <div class="flex-auto">
+      <title-bar>
+        <drag-region class="w-full"></drag-region>
+      </title-bar>
+      main
+    </div>
   </n-config-provider>
 </template>
---
+++ renderer/components/DragRegion.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="drag-region">
+    <slot><span class="_placeholder">_placeholder_</span></slot>
+  </div>
+</template>
+
+<style scoped>
+._placeholder {
+  opacity: 0;
+}
+</style>
